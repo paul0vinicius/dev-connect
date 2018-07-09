0af3e9f7ebe30c6f4e09e7bab03bcce7816c4533
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-09 07:37:38 -0300

    Add secret key to create token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://paulo:paulovinicius1@ds133041.mlab.com:33041/dev-connect-dacaproject"
+    "mongodb://paulo:paulovinicius1@ds133041.mlab.com:33041/dev-connect-dacaproject",
+  secretOrKey: "secret"
 };
