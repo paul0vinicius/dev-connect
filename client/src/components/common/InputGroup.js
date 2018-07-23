35aebe7d8204e806b972b14a3dbd5407216e41d7
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-22 21:39:34 -0300

    Fix className on InputGroup

--- client/src/components/common/InputGroup.js
+++ client/src/components/common/InputGroup.js
@@ -14,7 +14,7 @@ const InputGroup = ({
   return (
     <div className="input-group mb-3">
       <div className="input-group-prepend">
-        <span className="group-text">
+        <span className="input-group-text">
           <i className={icon} />
         </span>
       </div>
