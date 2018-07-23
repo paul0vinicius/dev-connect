87cb8b35af956832356bc1fec30b808d6eda7b17
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-22 23:40:15 -0300

    Hide keys from github

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://paulo:paulovinicius1@ds133041.mlab.com:33041/dev-connect-dacaproject",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
